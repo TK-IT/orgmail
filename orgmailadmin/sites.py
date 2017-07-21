a0fc89a9839a0d1fe1205f311ab6365a9a943247
Mathias Rav <rav@cs.au.dk>
2017-07-21 21:00:17 +0200

    Fix url patterns

--- orgmailadmin/sites.py
+++ orgmailadmin/sites.py
@@ -8,15 +8,15 @@ class OrgmailadminSite(object):
         urls = [
             url(r'^login/$', views.LoginView.as_view(), name='login'),
             url(r'^$', views.DomainList.as_view(), name='domain_list'),
-            url(r'^(?P<domain_name>[\w.]+)/details/$',
+            url(r'^(?P<domain_name>.+)/details/$',
                 views.DomainUpdate.as_view(), name='domain_update'),
-            url(r'^(?P<domain_name>[\w.]+)/$',
+            url(r'^(?P<domain_name>.+)/$',
                 views.AliasList.as_view(), name='alias_list'),
-            url(r'^(?P<domain_name>[\w.]+)/new/$',
+            url(r'^(?P<domain_name>.+)/new/$',
                 views.AliasCreate.as_view(), name='alias_create'),
-            url(r'^(?P<domain_name>[\w.]+)/alias/(?P<alias_name>[\w.]+)/$',
+            url(r'^(?P<domain_name>.+)/alias/(?P<alias_name>.+)/$',
                 views.AliasUpdate.as_view(), name='alias_update'),
-            url(r'^(?P<domain_name>[\w.]+)/alias/(?P<alias_name>[\w.]+)/delete/$',
+            url(r'^(?P<domain_name>.+)/alias/(?P<alias_name>.+)/delete/$',
                 views.AliasDelete.as_view(), name='alias_delete'),
         ]
         return urls
