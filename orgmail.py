9f29d02c70d0010e02d5c63bf24524f8b03ea65e
Mathias Rav <rav@cs.au.dk>
2017-07-21 15:56:10 +0200

    Close connection and check for catchall

--- orgmail.py
+++ orgmail.py
@@ -24,12 +24,21 @@ def translate_recipient(recipient):
         raise ValueError(recipient)
     models = import_orgmailadmin_models()
     try:
-        domain = models.Domain.objects.get(name=domain_part)
-    except models.Domain.DoesNotExist:
-        raise UnknownDomain(recipient)
-    try:
-        alias = models.Alias.objects.get(
-            domain=domain, name=local_part)
-    except models.Alias.DoesNotExist:
-        raise UnknownLocal(recipient)
-    return alias.recipient_list
+        try:
+            domain = models.Domain.objects.get(name=domain_part)
+        except models.Domain.DoesNotExist:
+            raise UnknownDomain(recipient)
+        try:
+            alias = models.Alias.objects.get(
+                domain=domain, name=local_part)
+        except models.Alias.DoesNotExist:
+            try:
+                # Catch-all
+                alias = models.Alias.objects.get(
+                    domain=domain, name=models.Alias.CATCHALL_NAME)
+            except models.Alias.DoesNotExist:
+                raise UnknownLocal(recipient)
+        return alias.recipient_list
+    finally:
+        from django.db import connection
+        connection.close()
