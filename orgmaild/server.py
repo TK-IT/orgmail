119d7e7773ef87f73a897aa0e6fd383d9114174c
Mathias Rav <rav@cs.au.dk>
2017-07-21 15:56:16 +0200

    Implement OrgmailForwarder

--- orgmaild/server.py
+++ orgmaild/server.py
@@ -3,4 +3,27 @@ from emailtunnel import (
 )
 from emailtunnel.mailhole import MailholeRelayMixin
 
-from orgmail import translate_recipient
+from orgmail import (
+    translate_recipient, UnknownDomain, UnknownLocal,
+)
+
+
+class OrgmailForwarder(SMTPForwarder, MailholeRelayMixin):
+    MAIL_FROM = 'admin@TAAGEKAMMERET.dk'
+
+    def get_mailhole_key(self):
+        return os.getenv('MAILHOLE_KEY')
+
+    def should_mailhole(self, message, recipient, sender):
+        # Send everything to mailhole
+        return True
+
+    def translate_recipient(self, rcptto):
+        try:
+            return translate_recipient(rcptto)
+        except (UnknownDomain, UnknownLocal) as exn:
+            raise InvalidRecipient(
+                '%s: %s' % (exn.__class__.__name__, exn))
+
+    def get_envelope_mailfrom(self, envelope, recipients=None):
+        return self.__class__.MAIL_FROM
